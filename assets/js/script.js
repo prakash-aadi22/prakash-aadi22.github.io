214ac9155dc793af2a808f72d1baf1c5cac55c31
Aditya Prakash <prakashaditya2209@gmail.com>
2025-02-08 17:47:41 +0530

    Update script.js

--- assets/js/script.js
+++ assets/js/script.js
@@ -619,7 +619,16 @@ function printDiv() {
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
 
     <style>
+@media print {
+  body {
+    -webkit-print-color-adjust: exact; /* Chrome/Safari */
+    print-color-adjust: exact; /* Firefox */
+  }
 
+  @page {
+    size: A4 portrait; /* Force portrait layout */
+  }
+}
     </style>
   </head>
   <body>
