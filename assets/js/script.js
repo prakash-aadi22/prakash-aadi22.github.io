778af05ae041c865e40bbcbea2e73976dacc0678
Aditya Prakash <prakashaditya2209@gmail.com>
2025-04-01 21:30:10 +0530

    filter

--- assets/js/script.js
+++ assets/js/script.js
@@ -54,10 +54,15 @@ overlay.addEventListener("click", testimonialsModalFunc);
 // custom select variables
 const select = document.querySelector("[data-select]");
 const selectItems = document.querySelectorAll("[data-select-item]");
-const selectValue = document.querySelector("[data-selecct-value]");
+const selectValue = document.querySelector("[data-select-value]");
 const filterBtn = document.querySelectorAll("[data-filter-btn]");
+const selectProject = document.querySelector("[data-select-project]");
 
 select.addEventListener("click", function () {
+  elementToggleFunc(this);
+});
+
+selectProject.addEventListener("click", function () {
   elementToggleFunc(this);
 });
 
@@ -66,34 +71,49 @@ for (let i = 0; i < selectItems.length; i++) {
   selectItems[i].addEventListener("click", function () {
     let selectedValue = this.innerText.toLowerCase();
     selectValue.innerText = this.innerText;
-    elementToggleFunc(select);
-    filterFunc(selectedValue);
+    // elementToggleFunc(select);
+    elementToggleFunc(selectProject);
+    // filterFunc(selectedValue);
+    filterFuncForProject(selectedValue);
   });
 }
 
 // filter variables
-const filterItems = document.querySelectorAll("[data-filter-item]");
+// const filterItems = document.querySelectorAll("[data-filter-item]");
+// const filterFunc = function (selectedValue) {
+//   for (let i = 0; i < filterItems.length; i++) {
+//     if (selectedValue === "all") {
+//       filterItems[i].classList.add("active");
+//     } else if (selectedValue === filterItems[i].dataset.category) {
+//       filterItems[i].classList.add("active");
+//     } else {
+//       filterItems[i].classList.remove("active");
+//     }
+//   }
+// };
 
-const filterFunc = function (selectedValue) {
-  for (let i = 0; i < filterItems.length; i++) {
+// filter Project
+const filterProject = document.querySelectorAll("[data-filter-project]");
+const filterFuncForProject = function (selectedValue) {
+  for (let i = 0; i < filterProject.length; i++) {
     if (selectedValue === "all") {
-      filterItems[i].classList.add("active");
-    } else if (selectedValue === filterItems[i].dataset.category) {
-      filterItems[i].classList.add("active");
+      filterProject[i].classList.add("active");
+    } else if (selectedValue === filterProject[i].dataset.category) {
+      filterProject[i].classList.add("active");
     } else {
-      filterItems[i].classList.remove("active");
+      filterProject[i].classList.remove("active");
     }
   }
 };
 
 // add event in all filter button items for large screen
 let lastClickedBtn = filterBtn[0];
-
 for (let i = 0; i < filterBtn.length; i++) {
   filterBtn[i].addEventListener("click", function () {
     let selectedValue = this.innerText.toLowerCase();
     selectValue.innerText = this.innerText;
-    filterFunc(selectedValue);
+    // filterFunc(selectedValue);
+    filterFuncForProject(selectedValue);
 
     lastClickedBtn.classList.remove("active");
     this.classList.add("active");
